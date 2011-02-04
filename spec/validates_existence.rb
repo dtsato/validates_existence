1537ee3047519a77f1878bef346f2daf59c94afe
Danilo Sato <dtsato@gmail.com>
2011-02-04 11:13:09 -0600

    Adding rspec macro

---
+++ spec/validates_existence.rb
@@ -0,0 +1,34 @@
+module ValidatesExistence
+  module TestCase
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+    
+    module ClassMethods
+      def should_validate_existence_of(*associations)
+        allow_nil = get_options!(associations, :allow_nil)
+
+        associations.each do |association|
+          it "requires #{association} exists" do
+            reflection = subject.class.reflect_on_association(association)
+            object = subject
+            object.send("#{reflection.primary_key_name}=", 0)
+            object.should_not be_valid
+            object.errors[association.to_sym].should include("does not exist")
+          end
+        end
+
+        if allow_nil
+          associations.each do |association|
+            it "allows #{association} to be nil" do
+              reflection = subject.class.reflect_on_association(association)
+              object = subject
+              object.send("#{reflection.primary_key_name}=", nil)
+              object.should be_valid
+            end
+          end
+        end
+      end
+    end
+  end
+end
